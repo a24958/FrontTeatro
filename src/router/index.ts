53b704015537bf8dca7aa717d35a4e7de6dff76c
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-27 20:04:59 +0100

    Create the IntranetView with some fake text, added to the router file and associate it in the header component with the word Romantica, this is provisional

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ContactView from '../views/ContactView.vue'
 import RentView from '../views/RentView.vue'
+import IntranteView from '../views/IntranetView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,17 +10,34 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta:{
+        showHeader: true,
+      }
     },
     {
       path: '/contact',
       name: 'contact',
       component: ContactView,
+      meta:{
+        showHeader: true,
+      }
     },
     {
       path: '/rent',
       name: 'rent',
       component: RentView,
+      meta:{
+        showHeader: true,
+      }
+    },
+    {
+      path: '/intranet',
+      name: 'intranet',
+      component: IntranteView,
+      meta:{
+        showHeader: false,
+      }
     },
   ]
 })
