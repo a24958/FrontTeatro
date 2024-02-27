547c6fbb3ad811a3a39b45fc432cfd1b91fd9a1d
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-27 16:46:54 +0100

    Delete utils folder and all it´s files

--- src/utils/utils.ts
+++
@@ -1,7 +0,0 @@
-import { toggleDropdown, toggleMenu } from "./MenuFunctions";
-import { currentSlide, plusSlides } from "./SliderFunctions";
-
-export const toggleMenuUtils = toggleMenu;
-export const toggleDropdownUtils = toggleDropdown;
-export const currentSlideUtils = currentSlide;
-export const plusSlidesUtils = plusSlides;
