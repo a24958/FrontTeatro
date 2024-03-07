7d10c3246d10b3320b00af6994c15c71b5d1c7ec
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-03-07 16:44:17 +0100

    Add greneric function to make all the api calls

---
+++ src/stores/stroreApiCalls.ts
@@ -0,0 +1,32 @@
+import { defineStore } from "pinia";
+
+export const apiCallsFunctionsStore = defineStore('apiCallsFunctions', () => {
+    async function apiCall(method:string, url:string, data?:any) {
+        const requestOptions: RequestInit = {
+            method: method, 
+            mode: 'cors', 
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        if(data){
+            requestOptions.body = JSON.stringify(data);
+        }
+    
+        try {
+            const response = await fetch(url, requestOptions);
+            
+            if (!response.ok) {
+                throw new Error('Error en la solicitud: ' + response.statusText);
+            }
+            
+            const json = await response.json();  
+            
+        } catch (error) {
+            console.log('Error al hacer la llamada a la API:', error);
+        }
+    }
+
+    return { apiCall }
+});
