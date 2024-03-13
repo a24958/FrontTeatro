f3252b8c336089341520d085a7d0f753f13a3f86
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-03-13 17:54:18 +0100

    Finaly finish the register functionality

--- src/components/registerViewComponents/RegisterForm.vue
+++ src/components/registerViewComponents/RegisterForm.vue
@@ -17,14 +17,19 @@ async function register() {
   const passwordRegister = password.value
 
   try {
-    const userReturned = await loginStore.postUser(userRegister, emailRegister, passwordRegister);
-    if(userReturned == true){
-      await router.push('/');
-    } else {
-      errorMessage.value = 'Error a la hora de registrar usuario';
-      return;
+    if (!user.value || !email.value || !password.value) {
+      errorMessage.value = 'Todos los campos son obligatorios';
+      return; 
     }
-  
+
+    const userReturned = await loginStore.postUser(userRegister, emailRegister, passwordRegister);
+    if (userReturned == true) {
+        await router.push('/');
+      } else {
+        errorMessage.value = 'Error a la hora de registrar usuario';
+        return;
+      }
+
   } catch (error) {
     errorMessage.value = 'Error a la hora de registrar usuario';
   }
@@ -33,36 +38,38 @@ async function register() {
 </script>
 
 <template>
-    <section class="login-section">
-        <h1>Register</h1>
-        <form class="login-formulario">
-            <label for="nombre">Nombre</label>
-            <input v-model="user" type="text" id="nombre" name="nombre">
+  <section class="login-section">
+    <h1>Register</h1>
+    <form class="login-formulario">
+      <label for="nombre">Nombre</label>
+      <input v-model="user" type="text" id="nombre" name="nombre" required>
 
-            <label for="email">Email</label>
-            <input v-model="email" type="text" id="email" name="email">
+      <label for="email">Email</label>
+      <input v-model="email" type="text" id="email" name="email" required>
 
-            <label for="contraseña">Contraseña</label>
-            <input v-model="password" type="password" id="contraseña" name="contraseña">
+      <label for="contraseña">Contraseña</label>
+      <input v-model="password" type="password" id="contraseña" name="contraseña" required>
 
-            <button id="primary" @click.prevent="register">Register</button>
-        </form>
-    </section>
+      <div class="error" v-if="errorMessage">{{ errorMessage }}</div>
+
+      <button id="primary" @click.prevent="register">Register</button>
+    </form>
+  </section>
 </template>
 
 <style scoped>
 .login-formulario #primary {
-    padding: 8px;
-    background-color: #3385D9;
-    width: 350px;
-    color: white;
-    font-size: 16px;
-    border-radius: 8px;
-    border-width: 1px;
-    border-style: solid;
-    border-color: #3385D9;
-    margin-top: 12px;
-    margin-bottom: 48px;
+  padding: 8px;
+  background-color: #3385D9;
+  width: 350px;
+  color: white;
+  font-size: 16px;
+  border-radius: 8px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #3385D9;
+  margin-top: 12px;
+  margin-bottom: 48px;
 }
 
 .login-formulario #primary a {
@@ -100,46 +107,53 @@ async function register() {
   border-style: solid;
   border-radius: 12px;
 }
-.login-formulario label, .login-formulario input {
+
+.login-formulario label,
+.login-formulario input {
   margin-top: 12px;
   display: flex;
   flex-direction: row;
   align-items: start;
   width: 280px;
 }
+
 .login-formulario input {
   border-radius: 8px;
   border-style: solid;
   border-color: #EBEBEB;
   padding: 8px;
 }
+
 .login-formulario input:focus {
   border: 1px solid #3385D9;
 }
+
 .login-formulario #primary {
   width: 300px;
   margin-top: 32px;
   margin-bottom: 24px;
 }
+
 .login-formulario #secondary {
   width: 300px;
   margin-top: 0px;
   margin-bottom: 24px;
 }
+
 .login-section {
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-bottom: 300px;
 }
+
 .login-section h1 {
   color: #3385D9;
   font-size: 48px;
 }
 
 @media screen and (min-width: 800px) {
-  .login-section{
+  .login-section {
     margin-bottom: 325px;
   }
-}
-</style>
+}</style>
