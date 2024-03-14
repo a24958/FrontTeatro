b7f142861f2aeca3f78ec7e4cc50e4801ce81a6b
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-03-14 19:50:51 +0100

    Install i18n an make an easy test

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,27 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 const pinia = createPinia()
 
+const i18n = createI18n({
+    locale: 'en',
+    messages: {
+      es: {
+        hello: "Hola"
+      },
+      en: {
+        hello: "Hello"
+      }
+    }
+  })
+
 app.use(router)
 app.use(pinia)
+app.use(i18n)
 
 app.mount('#app')
